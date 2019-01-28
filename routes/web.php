433609394e450d6f38afb8317f1a38ccd1ac30b8
cherimarcelina <cherimarcelina10@gmail.com>
2019-01-28 14:07:56 +0700

    tokap

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::prefix('admin')->group(function(){
+	Route::get('/', function(){
+		return view('admin.main');
+	})->name('admin.home');
+});
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');

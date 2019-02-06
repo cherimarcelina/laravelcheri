a97f98ae5bdfc22c3f7d898ed9edf178b1e7504d
cherimarcelina <cherimarcelina10@gmail.com>
2019-02-06 19:11:57 +0700

    tokap

--- resources/views/admin/pages/user/setting.blade.php
+++ resources/views/admin/pages/user/setting.blade.php
@@ -13,35 +13,47 @@
 
 						<div class="form-group form-label-group">
 							<input type="text" name="name"
-							class="form-control" value="{{ old('name', $dt->name)}}"
+							class="form-control {{$errors->has('name')?'is-invalid':''}}" value="{{ old('name', $dt->name)}}"
 							id="iName" placeholder="Name" required>
 							<label for="iName">Nama</label>
+							@if($errors->has('name'))
+							<div class="invalid-feedback">{{$errors->first('name')}}</div>
+							@endif
 							
 						</div>
 
 							<div class="form-group form-label-group">
 							<input type="text" name="email"
-							class="form-control" value="{{ old('email', $dt->email)}}"
+							class="form-control {{$errors->has('email')?'is-invalid':''}}" value="{{ old('email', $dt->email)}}"
 							id="iEmail" placeholder="Email" required>
 							<label for="iEmail">Email</label>
+							@if($errors->has('email'))
+							<div class="invalid-feedback">{{$errors->first('email')}}</div>
+							@endif
 							
 							</div>
 
 							<div class="form-group form-label-group">
-							<input type="password" name="pas"
-							class="form-control"
+							<input type="password" name="password"
+							class="form-control {{$errors->has('password')?'is-invalid':''}}"
 							id="iPas" placeholder="Password">
 							<label for="iPas">Password</label>
+							@if($errors->has('password'))
+							<div class="invalid-feedback">{{$errors->first('password')}}</div>
+							@endif
 								<div class="form-text text-muted">
 									<small>Kossongkan Password Anda jika tidak ingin mengubahnya!</small>
 								</div>
 							</div>
 
 							<div class="form-group form-label-group ">
-							<input type="password" name="repas"
-							class="form-control"
+							<input type="password" name="repassword"
+							class="form-control {{$errors->has('repassword')?'is-invalid':''}}"
 							id="iRePas" placeholder="Re Password">
 							<label for="iRePas"> Re Password</label>
+							@if($errors->has('repassword'))
+							<div class="invalid-feedback">{{$errors->first('repassword')}}</div>
+							@endif
 							</div>
 
 					</div>

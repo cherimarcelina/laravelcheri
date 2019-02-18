ccc175fd3123d62eb77118d467d9adc4f5ba83e8
cherimarcelina <cherimarcelina10@gmail.com>
2019-02-18 11:11:36 +0700

    eps-19 Delete Database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -83,4 +83,15 @@ class UserController extends Controller
     		return back()->with('result','fail');
     	}
     }
+
+    public function delete(Request $req)
+    {
+    	$result = User::find($req->id);
+    	if ( $result->delete() ) {
+    		
+    		return back()->with('result','delete');
+    	} else{
+    		return back()->with('result','fail-delete');
+    	}
+    }
 }
--- resources/views/admin/pages/user/daftar.blade.php
+++ resources/views/admin/pages/user/daftar.blade.php
@@ -22,6 +22,26 @@
 </div>
 @endif
 
+@if(session('result')== 'delete')
+<div class="alert alert-success alert-dismissible fade show">
+	<strong>Delete!</strong> Berhasil dihapus.
+	<button type="button" class="close" data-dismiss="alert">
+		&times;
+	</button>
+</div>
+@endif
+
+@if(session('result')== 'fail-delete')
+<div class="alert alert-danger alert-dismissible fade show">
+	<strong>Failed!</strong> Gagal dihapus.
+	<button type="button" class="close" data-dismiss="alert">
+		&times;
+	</button>
+</div>
+@endif
+
+
+
 <div class="row">
 	<div class="col-md-6 mb-3">
 		<a href="{{ route('admin.user.add') }}" class="btn btn-primary">[+] Tambah</a>
@@ -92,7 +112,7 @@
 
 		<div class="modal-body">
 			Apakah Anda Yakin Untuk Menghapusnya?
-			<form id="form-delete" method="post" action="#">
+			<form id="form-delete" method="post" action="{{ route('admin.user') }} ">
 				{{ csrf_field() }}
 				{{method_field('delete') }}
 				<input type="hidden" name="id" id="input-id">
@@ -120,7 +140,7 @@
 		});
 
 		$('.btn-delete').click(function(){
-			alert(  $('#input-id').val());
+			$('#form-delete').submit();
 		});
 
 	})

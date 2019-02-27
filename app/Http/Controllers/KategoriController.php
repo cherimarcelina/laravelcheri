36ae09527e18f795a26ecf662964a00f465f2b61
cherimarcelina <cherimarcelina10@gmail.com>
2019-02-27 20:28:07 +0700

    eps-25 Form Edit

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -37,4 +37,15 @@ class KategoriController extends Controller
             return back()->with('result','fail')->withInput();
         }
     }
+
+    public function edit($id)
+    {
+        $data = Kategori::where('id',$id)->first();
+        return view('admin.pages.kategori.edit',['rc'=>$data]);
+    }
+
+    public function update(Request $req)
+    {
+        return 'Fungsi Update';
+    }
 }
--- resources/views/admin/pages/kategori/daftar.blade.php
+++ resources/views/admin/pages/kategori/daftar.blade.php
@@ -42,7 +42,7 @@
 	<tr>
 		<td>{{ $dt->nama_kategori }}</td>
 		<td>
-			<a href="#" class="btn btn-success btn-sm">
+			<a href="{{route('admin.kategori.edit',['id'=>$dt->id])}}" class="btn btn-success btn-sm">
 				<i class="fa fa-w fa-edit"></i>
 			</a>
 			<button type="button" class="btn btn-danger btn-sm">

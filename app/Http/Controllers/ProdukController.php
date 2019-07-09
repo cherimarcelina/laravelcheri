d14d29ec595b28dedc7aa468b3722eba332f6497
cherimarcelina <cherimarcelina10@gmail.com>
2019-07-10 06:53:06 +0700

    eps 32 Daftar search page

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Produk;
 
 class ProdukController extends Controller
 {
     public function daftar(Request $req)
     {
-    	return view('admin.pages.produk.daftar');
+    	/*menghubungkan tabel dgn kategori*/
+    	$data = Produk::join('kategori','kategori.id','produk.id_kategori')
+    		->where('nama_produk','like',"%{$req->keyword}%")
+    		->select('produk.*','nama_kategori')
+    		->orderBy('updated_at','desc')
+    		->paginate(10);
+
+    	return view('admin.pages.produk.daftar',['data'=>$data]);
     }
 }
--- resources/views/admin/pages/produk/daftar.blade.php
+++ resources/views/admin/pages/produk/daftar.blade.php
@@ -3,4 +3,76 @@
 @section('content')
 <h1>Produk</h1>
 <hr>
+<!-- Baris TAmbah dan pencarian-->
+<div class="row">
+	<!-- Tombol Tambah -->
+	<div class="col-md-6 mb-3">
+		<a href="#" class="btn btn-primary">[+] Tambah</a>
+	</div>
+
+	<!-- form pencarian -->
+	<div class="col-md-6 mb-3">
+		<form method="GET" action="{{route('admin.produk') }}">
+			<div class="input-group">
+				<input type="text" name="keyword" class="form-control" value="{{request ('keyword')}}">
+				<div class="input-group-append">
+					<button type="submit" class="btn btn-primary">
+						Cari!
+					</button>
+				</div>
+			</div><!-- end input group -->
+		</form>
+	</div>
+</div><!-- End row -->
+
+<!-- Baris daftar data -->
+<table class="table table-striped mb-3">
+	<tr>
+		<th>Gambar</th><th>Produk</th><th>&nbsp;</th>
+	</tr>
+	@foreach($data as $dt)
+	<tr>
+	<!-- Kolom Gambar -->
+		<td>
+			<img src="{{url('storage/gambar-produk/'.$dt->gambar_produk)}}" width="65">
+		</td>
+	<!-- kolom data -->
+	<td>
+		<small class="text-muted">{{$dt->kode_produk}}</small> <br>
+		{{$dt->nama_produk}},
+		Harga Rp.{{number_format($dt->harga,0,',','.')}},
+		Jumlah Stok {{$dt->stok}} <br>
+		<small class="text-muted">{{$dt->nama_kategori}}</small>
+	</td>
+
+	<!-- kolom tombol -->
+	<td>
+		<!-- tombol edit-->
+		<a href="#" class="btn btn-success btn-sm">
+			<i class="fa fa-w fa-edit"></i>
+		</a>
+		<!-- tombol gambar-->
+		<a href="#" class="btn btn-info btn-sm">
+			<i class="fa fa-w fa-edit"></i>
+		</a>
+
+		<!-- TOMBOL HAPUS -->
+		<button type="button"
+		data-id="{{$dt->id}}"
+		class="btn btn-danger btn-sm btn-trash">
+			<i class="fa fa-w fa-trash"></i>
+		</button>
+
+
+	</td>
+
+	</tr>
+		
+	@endforeach
+</table>
+<!-- pagging/halaman-->
+{{
+	$data->appends(request()->only('keyword'))
+	->links('vendor.pagination.bootstrap-4')
+}}
 @endsection
